b1ad913113c8ddc93d61c39f4ce7b3e16433f61b
kawa1228 <sxa.1105bo@gmail.com>
2018-09-13 13:32:51 +0900

    keyframes追加

--- src/index.js
+++ src/index.js
@@ -3,52 +3,54 @@ const weatherData = forecasts.weatherData()
 
 const result = document.getElementById('result')
 
-const img = document.createElement('img')
-const p = document.createElement('p')
-p.setAttribute('id', 'title')
+class MakeData {
+    execute() {
+        result.appendChild(this.today())
+        result.appendChild(this.tomorrow())
+    }
+    today() {
+        const todayImg = weatherData[0].image.url
 
-const today = ()=> {
-    const todayImg = weatherData[0].image.url
+        const img = document.createElement('img')
+        const p = document.createElement('p')
+        p.setAttribute('id', 'title')
+    
+        img.src = todayImg
+        img.setAttribute('alt', '今日の天気画像')
+    
+        p.textContent = '今日の天気'
+        p.className = 'today'
 
-    img.src = todayImg
-    img.setAttribute('alt', '今日の天気画像')
+        const div = document.createElement('div')
+        div.className = 'today-wrapper'
 
-    p.textContent = '今日の天気'
-    p.className = 'today'
+        div.appendChild(img)
+        div.appendChild(p)
 
-    result.appendChild(img)
-    result.appendChild(p)
-}
+        return div
+    }
+    tomorrow() {
+        const　tomorrowImg = weatherData[1].image.url
 
-const tomorrow = ()=> {
-    const　tomorrowImg = weatherData[1].image.url
+        const img = document.createElement('img')
+        const p = document.createElement('p')
+        p.setAttribute('id', 'title')
+    
+        img.src = tomorrowImg
+        img.setAttribute('alt', '明日の天気画像')
+    
+        p.textContent = '明日の天気'
+        p.className = 'tomorrow'
+    
+        const div = document.createElement('div')
+        div.className = 'tomorrow-wrapper'
 
-    img.src = tomorrowImg
-    img.setAttribute('alt', '明日の天気画像')
-
-    p.textContent = '明日の天気'
-    p.className = 'tomorrow'
-
-    result.appendChild(img)
-    result.appendChild(p)
-}
-
-const checkWeather = ()=> {
-    if(document.getElementById('title').className === 'today') {
-        tomorrow()
-    } else {
-        today()
+        div.appendChild(img)
+        div.appendChild(p)
+        
+        return div
     }
 }
 
-let change
-const changeDay = ()=> {
-    change = setInterval(checkWeather, 2000)
-}
-
-today()
-changeDay()
-
-document.getElementById('stop-btn').addEventListener('click',()=> {
-    clearInterval(change)
-})
+const makeData = new MakeData()
+makeData.execute()
